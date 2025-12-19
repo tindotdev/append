8a9076e7fc2c80c7417d55680457b9462d53ff3c
Tin Dejphachon <tindejphachon@gmail.com>
2025-12-19 17:19:45 +0700

    Initialize TanstackRouter SPA with TailwindCSS

---
+++ packages/web/src/app.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <div className="bg-black text-white">Hello, World</div>;
+}
---
+++ packages/web/src/main.tsx
@@ -0,0 +1,49 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+import "./main.css";
+import { App } from "./app";
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: App,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+const router = createRouter({
+  routeTree,
+  context: {},
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+});
+
+const rootElement = document.getElementById("app");
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
