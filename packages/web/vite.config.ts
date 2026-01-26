5505f1ffdaede2f7454beb67b2af82950d52b2e6
Tin Dejphachon <tindejphachon@gmail.com>
2026-01-26 23:33:53 +0700

    feat(web): enable autocodesplitting

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,19 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [devtools(), TanStackRouterVite(), viteReact(), tailwindcss()],
+	plugins: [
+		devtools(),
+		viteReact(),
+		tailwindcss(),
+		tanstackRouter({
+			autoCodeSplitting: true,
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
